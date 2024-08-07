128ea57f6607f7c06bbf40d6194db97f41b2fcdc
v.tymoshenko <v.tymoshenko@disoft.dev>
2024-08-07 15:09:24 +0200

    sidebar

--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -1,13 +1,17 @@
 import Header from '@/app/components/Layout/Header/Header'
 import React from 'react'
+import Sidebar from '@/app/components/Layout/Sidebar/Sidebar'
 import './layout.sass'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='layout'>
-      <Header />
-      {children}
-    </main>
+    <div className='layout'>
+      <Sidebar className='layout__sidebar'/>
+      <div className='layout__content'>
+        <Header />
+        <main>{children}</main>
+      </div>
+    </div>
   )
 }
 
