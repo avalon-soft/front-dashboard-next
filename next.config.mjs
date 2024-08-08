c59b9ed3bd2c753e5c8a880f2b0c98476dbf3eec
v.tymoshenko <v.tymoshenko@disoft.dev>
2024-08-08 17:48:50 +0200

    added parament for next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  distDir: 'output'
+  distDir: '.output'
 }
 
 export default withNextIntl(nextConfig);
